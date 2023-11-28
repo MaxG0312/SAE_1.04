5a9a18478046fc4b9e352a5e833b1ee6fa2e9831
jgenitri-iut90 <146005235+jgenitri-iut90@users.noreply.github.com>
2023-11-28 15:50:53 +0100

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,34 +1,35 @@
-DROP TABLE IF EXISTS Saison, Culture, Variete;
+DROP TABLE IF EXISTS variete, culture, saison;
 
-CREATE TABLE Saison (
+CREATE TABLE saison (
     saison VARCHAR(31),
     PRIMARY KEY(saison)
 );
 
-CREATE TABLE Culture (
+CREATE TABLE culture (
     id_culture INT AUTO_INCREMENT,
     libelle_culture VARCHAR(255),
     PRIMARY KEY(id_culture)
 );
 
-CREATE TABLE Variete (
+CREATE TABLE variete (
     id_variete INT AUTO_INCREMENT,
     libelle_variete VARCHAR(255),
     saison VARCHAR(31),
     culture INT,
-    prix_kg DOUBLE,
+    prix_kg DECIMAL(4,2),
+    stock DECIMAL(4,1),
     PRIMARY KEY(id_variete),
-    FOREIGN KEY(saison) REFERENCES Saison(saison),
-    FOREIGN KEY(culture) REFERENCES Culture(id_culture)
+    FOREIGN KEY(saison) REFERENCES saison(saison),
+    FOREIGN KEY(culture) REFERENCES culture(id_culture)
 );
 
-INSERT INTO Saison VALUES
+INSERT INTO saison VALUES
    ('Printemps'),
    ('Ete'),
    ('Automne'),
    ('Hiver');
 
-INSERT INTO Culture VALUES
+INSERT INTO culture VALUES
     (1, 'Tomates'),
     (2, 'Pommes'),
     (3, 'Poires'),
@@ -36,8 +37,8 @@ INSERT INTO Culture VALUES
     (5, 'Blé'),
     (6, 'Carottes');
 
-INSERT INTO Variete VALUES
-    (1, 'Carottes de Nantes', 'Ete', 6, 1.5),
-    (2, 'Tomates cerises', 'Ete', 1, 5.67),
-    (3, 'Pommes Gala', 'Hiver', 2, 1.95),
-    (4, 'Poires Williams', 'Ete', 3, 3.99);
+INSERT INTO variete VALUES
+    (1, 'Carottes de Nantes', 'Ete', 6, 1.5, 74.5),
+    (2, 'Tomates cerises', 'Ete', 1, 5.67, 52.0),
+    (3, 'Pommes Gala', 'Hiver', 2, 1.95, 0),
+    (4, 'Poires Williams', 'Ete', 3, 3.99, 5.8);
